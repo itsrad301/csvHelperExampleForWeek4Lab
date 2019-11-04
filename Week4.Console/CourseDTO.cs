bcf9d8937fc811498250c9ed4a94e0c6f990c5f2
ITSPaul <powell.paul@itsligo.ie>
2019-11-04 12:06:42 +0000

    Adding some DTO functionality for Courses. Note Course data has some As s well as Ys for year identifiers

---
+++ Week4.Console/CourseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week4.Console
+{
+    public class CourseDTO
+    {
+        public string CourseCode { get; set; }
+        public string Year { get; set; }
+        public string Title { get; set; }
+    }
+}
--- Week4.Console/Program.cs
+++ Week4.Console/Program.cs
@@ -16,20 +16,28 @@ namespace Week4.Console
             List<Programme> programmes = new List<Programme>();
             List<Student> students = new List<Student>();
             // Get the current assembly
-            Assembly assembly = Assembly.GetExecutingAssembly();
-                // Assembly name and resource stored in assembly
-                string courseResourceName = "Week4.Console.Courses.csv";
-                programmes = Get<Programme>(courseResourceName);
-            string studentResourceName = "Week4.Console.StudentList1.csv";
-            students = Get<Student>(studentResourceName);
+            //Assembly assembly = Assembly.GetExecutingAssembly();
+            //    // Assembly name and resource stored in assembly
+            //    string courseResourceName = "Week4.Console.Courses.csv";
+            //    programmes = Get<Programme>(courseResourceName);
+            //string studentResourceName = "Week4.Console.StudentList1.csv";
+            //students = Get<Student>(studentResourceName);
 
             // Get the embedded resource from the assembly
-            foreach (var item in programmes)
-                System.Console.WriteLine("{0}", item.ToString());
+            //foreach (var item in programmes)
+            //    System.Console.WriteLine("{0}", item.ToString());
 
-            foreach(var item in students)
-                System.Console.WriteLine("{0}", String.Concat(new string[] {item.StudentID," ",item.FirstName," ", item.SecondName  }));
+            //foreach(var item in students)
+            //    System.Console.WriteLine("{0}", String.Concat(new string[] {item.StudentID," ",item.FirstName," ", item.SecondName  }));
 
+
+            // DTO example usage
+            List<Course> courses = new List<Course>();
+            courses = get_courses();
+            foreach (var item in courses)
+            {
+                System.Console.WriteLine("{0} {1}", item.Title, item.Year);
+            }
             System.Console.ReadKey();
 
         }
@@ -50,6 +58,31 @@ namespace Week4.Console
                 }
             }
         }
+
+        private static List<Course> get_courses()
+        {
+            // Get the list of DTO records from the resource
+            List<CourseDTO> cdto = Get<CourseDTO>("Week4.Console.Courses.csv");
+            List<Course> Courses = new List<Course>();
+            // iterate over the course DTO records and create course records for each one making the course year an intiger in the process
+            // Dummy val
+            int val;
+            cdto.ForEach(rec => {
+                Courses.Add(
+                  new Course
+                  {
+                      CourseCode = rec.CourseCode,
+                      Title = rec.Title,
+                      Year = Int32.TryParse(rec.Year.Trim('Y'), out val) ? val : (Int32.TryParse(rec.Year.Trim('A'), out val) ? val : 0)
+                  }
+                  );
+                  });
+            
+            return Courses;
+            
+        }
+
+        
     }
 
         
